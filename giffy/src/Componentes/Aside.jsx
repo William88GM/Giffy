dc5ee2dbf265534d71e4cf6b5f5d8521f9e6fa3e
Guillermo <guillermogabrielmartinezbarros@gmail.com>
2023-05-19 15:34:56 -0300

    Adding a menu

--- giffy/src/Componentes/Aside.jsx
+++ giffy/src/Componentes/Aside.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 import { useHistorySearch } from "../hooks/HistorySearch";
 
 export default function Aside() {
-	const { list, deleteItem } = useHistorySearch();
+  const { list, deleteItem } = useHistorySearch();
 
-	return (
-		<aside>
-			Busquedas recientes
-			<ul>
-				{list.map((el, i) => (
-					<li key={i}>
-						<button onClick={()=>deleteItem(i)}>X</button>
-						<Link onClick={window.scroll(0, 0)} to={el}>
-							{el}
-						</Link>
-					</li>
-				))}
-			</ul>
-		</aside>
-	);
+  return (
+    <aside>
+      Busquedas recientes
+      <ul>
+        {list.map((el, i) => (
+          <li key={i}>
+            <button onClick={() => deleteItem(i)}>X</button>
+            <Link onClick={window.scroll(0, 0)} to={el}>
+              {el}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
