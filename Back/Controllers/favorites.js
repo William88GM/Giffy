5f5ab999290baf9c403782610ef0f32c47aa6a10
Guillermo <guillermogabrielmartinezbarros@gmail.com>
2023-07-17 17:01:52 -0300

    Update favorites.js

--- Back/Controllers/favorites.js
+++ Back/Controllers/favorites.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { connectToMongo } from "../mongoDB_connection.js";
 import Axios from "axios";
+import { favoriteModel } from "../Models/favoriteModel.js";
 
 export const favoritesRouter = Router();
 
@@ -21,7 +22,12 @@ favoritesRouter.post("/:id", (req, res) => {
                     };
                 }) //----------------------------------------------------------
                 .then((obj) => {
-                    const favorite = newFavorite({ favoriteModel, obj });
+                    const favorite = new favoriteModel({
+                        original: obj.original,
+                        title: obj.title,
+                        id_giffy: obj.id_giffy,
+                        date: new Date(),
+                    });
 
                     favorite
                         .save()
